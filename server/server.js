91710dab4ac3aeadd2c86b83d25ecea730d37192
misterbisson <casey.bisson@gmail.com>
2015-03-03 15:31:47 +0000

    console logging because of slow /cpu and /disk requests

--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ var exec = require('shelljs').exec;
 
 server.get('/disk', disk);
 function disk(req, res, next) {
+	console.log('/disk request');
 	var thing = exec("$((dd bs=1M count=1024 if=/dev/zero of=~/simple-container-benchmarks-writetest conv=fdatasync) 2>&1 | tail -1 | sed -e 's/^ *//' -e 's/ *$//')", {silent:true}).output;
-	var thing = exec("rm ~/simple-container-benchmarks-writetest", {silent:true}).output;
+	exec("rm ~/simple-container-benchmarks-writetest", {silent:true}).output;
+
+	console.log(thing);
 
 	res.setHeader('content-type', 'text/plain');
 	res.send(thing);
@@ -14,7 +17,10 @@ function disk(req, res, next) {
 
 server.get('/cpu', cpu);
 function cpu(req, res, next) {
-	var thing = exec("$((dd if=/dev/urandom bs=1M count=256 | md5sum) 2>&1 >/dev/null | tail -1 | sed -e 's/^ *//' -e 's/ *$//')", {silent:true}).output;
+	console.log('/cpu request');
+	var thing = exec("$((dd if=/dev/urandom bs=1M count=1 | md5sum) 2>&1 >/dev/null | tail -1 | sed -e 's/^ *//' -e 's/ *$//')", {silent:true}).output;
+
+	console.log(thing);
 
 	res.setHeader('content-type', 'text/plain');
 	res.send(thing);
@@ -23,8 +29,12 @@ function cpu(req, res, next) {
 
 server.get('/info', info);
 function info(req, res, next) {
+	console.log('/info request');
+
 	var mem = exec("free | head -2", {silent:true}).output;
 	var cpu = exec("lscpu", {silent:true}).output;
+
+	console.log(mem + cpu);
 
 	res.setHeader('content-type', 'text/plain');
 	res.send(mem + cpu);
